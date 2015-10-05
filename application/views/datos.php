e8448bafdf93cbd93c395454228c1acc7735bc05
lapv20 <lapv1992@icloud.com>
2015-10-05 13:54:56 -0430

    View Datos 2.1

--- application/views/datos.php
+++ application/views/datos.php
@@ -14,7 +14,7 @@
 		</div>
 		<div class="container">
 		  <!-- Nav tabs -->
-		  <ul class="nav nav-tabs" role="tablist">
+		  <ul class="nav nav-pills nav-justified" role="tablist">
 		    <li role="presentation" class="active"><a href="#dia1" aria-controls="dia1" role="tab" data-toggle="tab">Día 1</a></li>
 		    <li role="presentation"><a href="#dia2" aria-controls="dia2" role="tab" data-toggle="tab">Día 2</a></li>
 		    <li role="presentation"><a href="#dia3" aria-controls="dia3" role="tab" data-toggle="tab">Día 3</a></li>
@@ -26,21 +26,39 @@
 		  <div class="tab-content">
 		    <div role="tabpanel" class="tab-pane active" id="dia1">
 		    	<div class="row">
-		    		<div class="col-md-6">
-						<div class="panel panel-info">
+		    		<div class="col-lg-6 col-sm-6 col-xs-12">
+						<div class="panel panel-primary">
 							<div class="panel-heading">Día 1<small id="rigth">Martes, 04 de Agosto 2015</small></div>
 							<div class="panel-body">
-								<table id="firstTable" class="table">
+								<table id="firstTable" class="table table-condensed table-bordered">
 									<tbody>
-										<tr><td>Cola</td><td>del lado del estacionamiento</td></tr>
-										<tr><td>Apertura del Comedor</td><td>12:09:00</td></tr>
-										<tr><td>Inicio Captura de Datos</td><td>11:55:00</td></tr>
-										<tr><td>Final Captura de Datos</td><td>12:52:00</td></tr>
-										<tr><td>Clientes en cola a las 11:57:00:</td><td>123</td></tr>
-										<tr><td>Número de Servidores:</td><td>2</td></tr>
+										<tr>
+											<th>Cola</th>
+											<th>Apertura del Comedor</th>
+										</tr>
+										<tr>
+											<td>del lado del estacionamiento</td>
+											<td>12:09:00</td>
+										</tr>
+										<tr>
+											<th>Inicio Captura de Datos</th>
+											<th>Final Captura de Datos</th>
+										</tr>
+										<tr>
+											<td>11:55:00</td>
+											<td>12:52:00</td>
+										</tr>
+										<tr>
+											<th>Clientes en cola a las 11:57:00</th>
+											<th>Número de Servidores</th>
+										</tr>
+										<tr>
+											<td>123</td>
+											<td>2</td>
+										</tr>
 									</tbody>
 								</table>
-								<div id="tabla_ajustada"><table class="table">
+								<div id="tabla_ajustada"><table class="table table-condensed">
 									<thead>
 										<th colspan="2"><span class="glyphicon glyphicon-menu-left" aria-hidden="true"></span>Intervalos<span class="glyphicon glyphicon-menu-right" aria-hidden="true"></span></th>
 										<th>Núm Clientes	<span class="glyphicon glyphicon-user" aria-hidden="true"></span></th>
@@ -72,40 +90,58 @@
 		    </div>
 		    <div role="tabpanel" class="tab-pane" id="dia2">
 		    	<div class="row">
-		    		<div class="col-md-6">
-						<div class="panel panel-info">
+		    		<div class="col-lg-6 col-sm-6 col-xs-12">
+						<div class="panel panel-primary">
 							<div class="panel-heading">Día 2 <small id="rigth">Miércoles, 05 de Agosto 2015</small></div>
 							<div class="panel-body">
-								<table id="firstTable" class="table">
+								<table id="firstTable" class="table table-condensed table-bordered">
 									<tbody>
-										<tr><td>Cola</td><td></td></tr>
-										<tr><td>Apertura del Comedor</td><td></td></tr>
-										<tr><td>Inicio Captura de Datos</td><td></td></tr>
-										<tr><td>Final Captura de Datos</td><td></td></tr>
-										<tr><td>Clientes en cola a las 00:00:00:</td><td></td></tr>
-										<tr><td>Número de Servidores:</td><td></td></tr>
+										<tr>
+											<th>Cola</th>
+											<th>Apertura del Comedor</th>
+										</tr>
+										<tr>
+											<td>del lado del estacionamiento</td>
+											<td>12:09:00</td>
+										</tr>
+										<tr>
+											<th>Inicio Captura de Datos</th>
+											<th>Final Captura de Datos</th>
+										</tr>
+										<tr>
+											<td>11:55:00</td>
+											<td>12:52:00</td>
+										</tr>
+										<tr>
+											<th>Clientes en cola a las 11:57:00</th>
+											<th>Número de Servidores</th>
+										</tr>
+										<tr>
+											<td>123</td>
+											<td>2</td>
+										</tr>
 									</tbody>
 								</table>
-								<div id="tabla_ajustada"><table class="table">
+								<div id="tabla_ajustada"><table class="table table-condensed">
 									<thead>
 										<th colspan="2"><span class="glyphicon glyphicon-menu-left" aria-hidden="true"></span>Intervalos<span class="glyphicon glyphicon-menu-right" aria-hidden="true"></span></th>
 										<th>Núm Clientes	<span class="glyphicon glyphicon-user" aria-hidden="true"></span></th>
 									</thead>
 									<tbody>
-										<tr><td>00:00:00</td><td>00:00:00</td><td>###</td></tr>
-										<tr><td>00:00:00</td><td>00:00:00</td><td>###</td></tr>
-										<tr><td>00:00:00</td><td>00:00:00</td><td>###</td></tr>
-										<tr><td>00:00:00</td><td>00:00:00</td><td>###</td></tr>
-										<tr><td>00:00:00</td><td>00:00:00</td><td>###</td></tr>
-										<tr><td>00:00:00</td><td>00:00:00</td><td>###</td></tr>
-										<tr><td>00:00:00</td><td>00:00:00</td><td>###</td></tr>
-										<tr><td>00:00:00</td><td>00:00:00</td><td>###</td></tr>
-										<tr><td>00:00:00</td><td>00:00:00</td><td>###</td></tr>
-										<tr><td>00:00:00</td><td>00:00:00</td><td>###</td></tr>
-										<tr><td>00:00:00</td><td>00:00:00</td><td>###</td></tr>
-										<tr><td>00:00:00</td><td>00:00:00</td><td>###</td></tr>
-										<tr><td>00:00:00</td><td>00:00:00</td><td>###</td></tr>
-										<tr><td colspan="2">Media</td><td># Clientes cada ## min</td></tr>
+										<tr><td>11:55:00</td><td>12:00:00</td><td>16</td></tr>
+										<tr><td>12:00:00</td><td>12:05:00</td><td>20</td></tr>
+										<tr><td>12:05:00</td><td>12:10:00</td><td>10</td></tr>
+										<tr><td>12:10:00</td><td>12:15:00</td><td>18</td></tr>
+										<tr><td>12:15:00</td><td>12:20:00</td><td>33</td></tr>
+										<tr><td>12:20:00</td><td>12:25:00</td><td>27</td></tr>
+										<tr><td>12:25:00</td><td>12:30:00</td><td>30</td></tr>
+										<tr><td>12:30:00</td><td>12:35:00</td><td>18</td></tr>
+										<tr><td>12:35:00</td><td>12:40:00</td><td>27</td></tr>
+										<tr><td>12:40:00</td><td>12:45:00</td><td>8</td></tr>
+										<tr><td>12:45:00</td><td>12:50:00</td><td>21</td></tr>
+										<tr><td>12:50:00</td><td>12:52:00</td><td>18</td></tr>
+										<tr><td>12:55:00</td><td>01:00:00</td><td>-</td></tr>
+										<tr><td colspan="2">Media</td><td>19 Clientes cada 5 min</td></tr>
 									</tbody>
 								</table></div>
 							</div>
@@ -118,40 +154,58 @@
 		    </div>
 		    <div role="tabpanel" class="tab-pane" id="dia3">
 		    	<div class="row">
-		    		<div class="col-md-6">
-						<div class="panel panel-info">
+		    		<div class="col-lg-6 col-sm-6 col-xs-12">
+						<div class="panel panel-primary">
 							<div class="panel-heading">Día 3 <small id="rigth">Jueves, 06 de Agosto 2015</small></div>
 							<div class="panel-body">
-								<table id="firstTable" class="table">
+								<table id="firstTable" class="table table-condensed table-bordered">
 									<tbody>
-										<tr><td>Cola</td><td></td></tr>
-										<tr><td>Apertura del Comedor</td><td></td></tr>
-										<tr><td>Inicio Captura de Datos</td><td></td></tr>
-										<tr><td>Final Captura de Datos</td><td></td></tr>
-										<tr><td>Clientes en cola a las 00:00:00:</td><td></td></tr>
-										<tr><td>Número de Servidores:</td><td></td></tr>
+										<tr>
+											<th>Cola</th>
+											<th>Apertura del Comedor</th>
+										</tr>
+										<tr>
+											<td>del lado del estacionamiento</td>
+											<td>12:09:00</td>
+										</tr>
+										<tr>
+											<th>Inicio Captura de Datos</th>
+											<th>Final Captura de Datos</th>
+										</tr>
+										<tr>
+											<td>11:55:00</td>
+											<td>12:52:00</td>
+										</tr>
+										<tr>
+											<th>Clientes en cola a las 11:57:00</th>
+											<th>Número de Servidores</th>
+										</tr>
+										<tr>
+											<td>123</td>
+											<td>2</td>
+										</tr>
 									</tbody>
 								</table>
-								<div id="tabla_ajustada"><table class="table">
+								<div id="tabla_ajustada"><table class="table table-condensed">
 									<thead>
 										<th colspan="2"><span class="glyphicon glyphicon-menu-left" aria-hidden="true"></span>Intervalos<span class="glyphicon glyphicon-menu-right" aria-hidden="true"></span></th>
 										<th>Núm Clientes	<span class="glyphicon glyphicon-user" aria-hidden="true"></span></th>
 									</thead>
 									<tbody>
-										<tr><td>00:00:00</td><td>00:00:00</td><td>###</td></tr>
-										<tr><td>00:00:00</td><td>00:00:00</td><td>###</td></tr>
-										<tr><td>00:00:00</td><td>00:00:00</td><td>###</td></tr>
-										<tr><td>00:00:00</td><td>00:00:00</td><td>###</td></tr>
-										<tr><td>00:00:00</td><td>00:00:00</td><td>###</td></tr>
-										<tr><td>00:00:00</td><td>00:00:00</td><td>###</td></tr>
-										<tr><td>00:00:00</td><td>00:00:00</td><td>###</td></tr>
-										<tr><td>00:00:00</td><td>00:00:00</td><td>###</td></tr>
-										<tr><td>00:00:00</td><td>00:00:00</td><td>###</td></tr>
-										<tr><td>00:00:00</td><td>00:00:00</td><td>###</td></tr>
-										<tr><td>00:00:00</td><td>00:00:00</td><td>###</td></tr>
-										<tr><td>00:00:00</td><td>00:00:00</td><td>###</td></tr>
-										<tr><td>00:00:00</td><td>00:00:00</td><td>###</td></tr>
-										<tr><td colspan="2">Media</td><td># Clientes cada ## min</td></tr>
+										<tr><td>11:55:00</td><td>12:00:00</td><td>16</td></tr>
+										<tr><td>12:00:00</td><td>12:05:00</td><td>20</td></tr>
+										<tr><td>12:05:00</td><td>12:10:00</td><td>10</td></tr>
+										<tr><td>12:10:00</td><td>12:15:00</td><td>18</td></tr>
+										<tr><td>12:15:00</td><td>12:20:00</td><td>33</td></tr>
+										<tr><td>12:20:00</td><td>12:25:00</td><td>27</td></tr>
+										<tr><td>12:25:00</td><td>12:30:00</td><td>30</td></tr>
+										<tr><td>12:30:00</td><td>12:35:00</td><td>18</td></tr>
+										<tr><td>12:35:00</td><td>12:40:00</td><td>27</td></tr>
+										<tr><td>12:40:00</td><td>12:45:00</td><td>8</td></tr>
+										<tr><td>12:45:00</td><td>12:50:00</td><td>21</td></tr>
+										<tr><td>12:50:00</td><td>12:52:00</td><td>18</td></tr>
+										<tr><td>12:55:00</td><td>01:00:00</td><td>-</td></tr>
+										<tr><td colspan="2">Media</td><td>19 Clientes cada 5 min</td></tr>
 									</tbody>
 								</table></div>
 							</div>
@@ -164,40 +218,58 @@
 		    </div>
 		    <div role="tabpanel" class="tab-pane" id="dia4">
 		    	<div class="row">
-		    		<div class="col-md-6">
-						<div class="panel panel-info">
-							<div class="panel-heading">Día 4 <small id="rigth">Martes, 04 de Agosto 2015</small></div>
+		    		<div class="col-lg-6 col-sm-6 col-xs-12">
+						<div class="panel panel-primary">
+							<div class="panel-heading">Día 4 <small id="rigth">Viernes, 07 de Agosto 2015</small></div>
 							<div class="panel-body">
-								<table id="firstTable" class="table">
+								<table id="firstTable" class="table table-condensed table-bordered">
 									<tbody>
-										<tr><td>Cola</td><td></td></tr>
-										<tr><td>Apertura del Comedor</td><td></td></tr>
-										<tr><td>Inicio Captura de Datos</td><td></td></tr>
-										<tr><td>Final Captura de Datos</td><td></td></tr>
-										<tr><td>Clientes en cola a las 00:00:00:</td><td></td></tr>
-										<tr><td>Número de Servidores:</td><td></td></tr>
+										<tr>
+											<th>Cola</th>
+											<th>Apertura del Comedor</th>
+										</tr>
+										<tr>
+											<td>del lado del estacionamiento</td>
+											<td>12:09:00</td>
+										</tr>
+										<tr>
+											<th>Inicio Captura de Datos</th>
+											<th>Final Captura de Datos</th>
+										</tr>
+										<tr>
+											<td>11:55:00</td>
+											<td>12:52:00</td>
+										</tr>
+										<tr>
+											<th>Clientes en cola a las 11:57:00</th>
+											<th>Número de Servidores</th>
+										</tr>
+										<tr>
+											<td>123</td>
+											<td>2</td>
+										</tr>
 									</tbody>
 								</table>
-								<div id="tabla_ajustada"><table class="table">
+								<div id="tabla_ajustada"><table class="table table-condensed">
 									<thead>
 										<th colspan="2"><span class="glyphicon glyphicon-menu-left" aria-hidden="true"></span>Intervalos<span class="glyphicon glyphicon-menu-right" aria-hidden="true"></span></th>
 										<th>Núm Clientes	<span class="glyphicon glyphicon-user" aria-hidden="true"></span></th>
 									</thead>
 									<tbody>
-										<tr><td>00:00:00</td><td>00:00:00</td><td>###</td></tr>
-										<tr><td>00:00:00</td><td>00:00:00</td><td>###</td></tr>
-										<tr><td>00:00:00</td><td>00:00:00</td><td>###</td></tr>
-										<tr><td>00:00:00</td><td>00:00:00</td><td>###</td></tr>
-										<tr><td>00:00:00</td><td>00:00:00</td><td>###</td></tr>
-										<tr><td>00:00:00</td><td>00:00:00</td><td>###</td></tr>
-										<tr><td>00:00:00</td><td>00:00:00</td><td>###</td></tr>
-										<tr><td>00:00:00</td><td>00:00:00</td><td>###</td></tr>
-										<tr><td>00:00:00</td><td>00:00:00</td><td>###</td></tr>
-										<tr><td>00:00:00</td><td>00:00:00</td><td>###</td></tr>
-										<tr><td>00:00:00</td><td>00:00:00</td><td>###</td></tr>
-										<tr><td>00:00:00</td><td>00:00:00</td><td>###</td></tr>
-										<tr><td>00:00:00</td><td>00:00:00</td><td>###</td></tr>
-										<tr><td colspan="2">Media</td><td># Clientes cada ## min</td></tr>
+										<tr><td>11:55:00</td><td>12:00:00</td><td>16</td></tr>
+										<tr><td>12:00:00</td><td>12:05:00</td><td>20</td></tr>
+										<tr><td>12:05:00</td><td>12:10:00</td><td>10</td></tr>
+										<tr><td>12:10:00</td><td>12:15:00</td><td>18</td></tr>
+										<tr><td>12:15:00</td><td>12:20:00</td><td>33</td></tr>
+										<tr><td>12:20:00</td><td>12:25:00</td><td>27</td></tr>
+										<tr><td>12:25:00</td><td>12:30:00</td><td>30</td></tr>
+										<tr><td>12:30:00</td><td>12:35:00</td><td>18</td></tr>
+										<tr><td>12:35:00</td><td>12:40:00</td><td>27</td></tr>
+										<tr><td>12:40:00</td><td>12:45:00</td><td>8</td></tr>
+										<tr><td>12:45:00</td><td>12:50:00</td><td>21</td></tr>
+										<tr><td>12:50:00</td><td>12:52:00</td><td>18</td></tr>
+										<tr><td>12:55:00</td><td>01:00:00</td><td>-</td></tr>
+										<tr><td colspan="2">Media</td><td>19 Clientes cada 5 min</td></tr>
 									</tbody>
 								</table></div>
 							</div>
@@ -210,40 +282,58 @@
 		    </div>
 		    <div role="tabpanel" class="tab-pane" id="dia5">
 		    	<div class="row">
-					<div class="col-md-6">
-						<div class="panel panel-info">
-							<div class="panel-heading">Día 5 <small id="rigth">Martes, 04 de Agosto 2015</small></div>
+					<div class="col-lg-6 col-sm-6 col-xs-12">
+						<div class="panel panel-primary">
+							<div class="panel-heading">Día 5 <small id="rigth">Jueves, 01 de Octubre 2015</small></div>
 							<div class="panel-body">
-								<table id="firstTable" class="table">
+								<table id="firstTable" class="table table-condensed table-bordered">
 									<tbody>
-										<tr><td>Cola</td><td></td></tr>
-										<tr><td>Apertura del Comedor</td><td></td></tr>
-										<tr><td>Inicio Captura de Datos</td><td></td></tr>
-										<tr><td>Final Captura de Datos</td><td></td></tr>
-										<tr><td>Clientes en cola a las 00:00:00:</td><td></td></tr>
-										<tr><td>Número de Servidores:</td><td></td></tr>
+										<tr>
+											<th>Cola</th>
+											<th>Apertura del Comedor</th>
+										</tr>
+										<tr>
+											<td>del lado del estacionamiento</td>
+											<td>12:09:00</td>
+										</tr>
+										<tr>
+											<th>Inicio Captura de Datos</th>
+											<th>Final Captura de Datos</th>
+										</tr>
+										<tr>
+											<td>11:55:00</td>
+											<td>12:52:00</td>
+										</tr>
+										<tr>
+											<th>Clientes en cola a las 11:57:00</th>
+											<th>Número de Servidores</th>
+										</tr>
+										<tr>
+											<td>123</td>
+											<td>2</td>
+										</tr>
 									</tbody>
 								</table>
-								<div id="tabla_ajustada"><table class="table">
+								<div id="tabla_ajustada"><table class="table table-condensed">
 									<thead>
 										<th colspan="2"><span class="glyphicon glyphicon-menu-left" aria-hidden="true"></span>Intervalos<span class="glyphicon glyphicon-menu-right" aria-hidden="true"></span></th>
 										<th>Núm Clientes	<span class="glyphicon glyphicon-user" aria-hidden="true"></span></th>
 									</thead>
 									<tbody>
-										<tr><td>00:00:00</td><td>00:00:00</td><td>###</td></tr>
-										<tr><td>00:00:00</td><td>00:00:00</td><td>###</td></tr>
-										<tr><td>00:00:00</td><td>00:00:00</td><td>###</td></tr>
-										<tr><td>00:00:00</td><td>00:00:00</td><td>###</td></tr>
-										<tr><td>00:00:00</td><td>00:00:00</td><td>###</td></tr>
-										<tr><td>00:00:00</td><td>00:00:00</td><td>###</td></tr>
-										<tr><td>00:00:00</td><td>00:00:00</td><td>###</td></tr>
-										<tr><td>00:00:00</td><td>00:00:00</td><td>###</td></tr>
-										<tr><td>00:00:00</td><td>00:00:00</td><td>###</td></tr>
-										<tr><td>00:00:00</td><td>00:00:00</td><td>###</td></tr>
-										<tr><td>00:00:00</td><td>00:00:00</td><td>###</td></tr>
-										<tr><td>00:00:00</td><td>00:00:00</td><td>###</td></tr>
-										<tr><td>00:00:00</td><td>00:00:00</td><td>###</td></tr>
-										<tr><td colspan="2">Media</td><td># Clientes cada ## min</td></tr>
+										<tr><td>11:55:00</td><td>12:00:00</td><td>16</td></tr>
+										<tr><td>12:00:00</td><td>12:05:00</td><td>20</td></tr>
+										<tr><td>12:05:00</td><td>12:10:00</td><td>10</td></tr>
+										<tr><td>12:10:00</td><td>12:15:00</td><td>18</td></tr>
+										<tr><td>12:15:00</td><td>12:20:00</td><td>33</td></tr>
+										<tr><td>12:20:00</td><td>12:25:00</td><td>27</td></tr>
+										<tr><td>12:25:00</td><td>12:30:00</td><td>30</td></tr>
+										<tr><td>12:30:00</td><td>12:35:00</td><td>18</td></tr>
+										<tr><td>12:35:00</td><td>12:40:00</td><td>27</td></tr>
+										<tr><td>12:40:00</td><td>12:45:00</td><td>8</td></tr>
+										<tr><td>12:45:00</td><td>12:50:00</td><td>21</td></tr>
+										<tr><td>12:50:00</td><td>12:52:00</td><td>18</td></tr>
+										<tr><td>12:55:00</td><td>01:00:00</td><td>-</td></tr>
+										<tr><td colspan="2">Media</td><td>19 Clientes cada 5 min</td></tr>
 									</tbody>
 								</table></div>
 							</div>
@@ -263,14 +353,7 @@
 	<link rel="stylesheet" type="text/css" href="<?php echo base_url('css/bootstrap.css');?>">
 	<style type="text/css">
 		#space{ padding-top: 20px; } #rigth{ float: right; }
-		#firstTable tbody tr td:first-child {
-			text-align: left;
-			font-weight: bold;
-		}
-		#firstTable tbody tr td:last-child {
-			text-align: right;
-			font-style: oblique;
-		}
 		.whitespace{ margin-top: 10px;}
+		table tr td { text-align: left; }
 </body>
 </html>
